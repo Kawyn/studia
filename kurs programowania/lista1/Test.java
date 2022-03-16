3442d090dcbb7673274d52a3761ebf7b36d255ac
Kawyn <kewinignasiak.2001@gmail.com>
2022-03-16 22:26:49 +0100

    Update Test.java

--- kurs programowania/lista1/Test.java
+++ kurs programowania/lista1/Test.java
@@ -2,6 +2,11 @@ public class Test {
 
     public static void main(String[] args) {
 
+        if (args.length == 0)
+        {
+            System.out.println("za malo argumentow");
+            System.exit(-1);
+        }
         LiczbyPierwsze liczbyPierwsze = null;
 
         try {
